c4d049b6e1980fc157ce5779097ef06ed3624cd5
Fernfrost <martin.nilsson@fernfrost.com>
2018-08-23 01:26:44 +0200

    gets definition

--- agent-capabilities-task/index.ts
+++ agent-capabilities-task/index.ts
@@ -1,4 +1,34 @@
 import task = require('vsts-task-lib/task');
+import * as rc from 'typed-rest-client/RestClient';
+
+interface TaskAgentPoolReference {
+    id: number;
+    isHosted: boolean;
+    name: string;
+}
+
+interface AgentPoolQueue {
+    pool: TaskAgentPoolReference;
+}
+
+interface BuildDefinition {
+    queue: AgentPoolQueue;
+}
+
+function getRequestOptions(options: any): any {
+    var baseOptions = {
+        auth: {
+            bearer: task.getVariable('System.AccessToken')
+        },
+        json: true,
+        qs: {},
+        rejectUnauthorized: false
+    }
+    options = Object.assign(baseOptions, options);
+    options.qs = Object.assign(options.qs, { 'api-version': '4.1' });
+
+    return options;
+}
 
 async function run() {
     try {
@@ -6,6 +36,21 @@ async function run() {
         if (!enableAccessToken || !(enableAccessToken.toLowerCase() === 'true')) {
             throw new Error('\'Allow scripts to access OAuth token\' must be enabled.');
         }
+
+        let baseUri = task.getVariable('system.teamFoundationServerUri');
+        let projectId = task.getVariable('system.teamProjectId');
+        let definitionId = task.getVariable('system.definitionId');
+
+        let definitionUri = `${baseUri}${projectId}/_apis/build/definitions/${definitionId}`;
+        task.debug(`definitionUri=${definitionUri}`);
+
+        let rest: rc.RestClient = new rc.RestClient('agent-capabilities');
+
+        var definitionOptions = getRequestOptions({});
+
+        let definitionResponse : rc.IRestResponse<BuildDefinition> = await rest.get<BuildDefinition>(definitionUri, definitionOptions);
+
+        task.debug(definitionResponse.statusCode.toString());
 
         task.setResult(task.TaskResult.Succeeded, 'Succeeded');
     }
