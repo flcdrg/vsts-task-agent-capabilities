b657bce4317e40e4bb50b4f98d90570aebb35154
Fernfrost <martin.nilsson@fernfrost.com>
2018-08-23 13:22:19 +0200

    uses vso node api

--- agent-capabilities-task/index.ts
+++ agent-capabilities-task/index.ts
@@ -1,5 +1,6 @@
 import task = require('vsts-task-lib/task');
 import * as rpn from 'request-promise-native';
+import * as vsts from 'vso-node-api';
 
 function getRequestOptions(options: any): any {
     var baseOptions = {
@@ -19,22 +20,23 @@ function getRequestOptions(options: any): any {
 
 async function run() {
     try {
-        let enableAccessToken = task.getVariable('system.enableAccessToken');
-        if (!enableAccessToken || !(enableAccessToken.toLowerCase() === 'true')) {
-            throw new Error('\'Allow scripts to access OAuth token\' must be enabled.');
-        }
+        //let enableAccessToken = task.getVariable('system.enableAccessToken');
+        //if (!enableAccessToken || !(enableAccessToken.toLowerCase() === 'true')) {
+        //    throw new Error('\'Allow scripts to access OAuth token\' must be enabled.');
+        //}
 
-        let baseUri = task.getVariable('system.teamFoundationServerUri');
+        //let baseUri = task.getVariable('system.teamFoundationCollectionUri');
+        let collectionUri = task.getVariable('system.teamFoundationCollectionUri');
         let projectId = task.getVariable('system.teamProjectId');
         //let definitionId = task.getVariable('system.definitionId');
-        let buildId = task.getVariable('build.buildId');
+        let buildId: number = Number(task.getVariable('build.buildId'));
 
         //let definitionUri = `${baseUri}${projectId}/_apis/build/definitions/${definitionId}`;
-        let buildUri = `${baseUri}${projectId}/_apis/build/builds/${buildId}`;
+        //let buildUri = `${baseUri}${projectId}/_apis/build/builds/${buildId}`;
         //task.debug(`definitionUri=${definitionUri}`);
-        task.debug(`buildUri=${buildUri}`);
+        //task.debug(`buildUri=${buildUri}`);
 
-        var buildOptions = getRequestOptions(
+        /*var buildOptions = getRequestOptions(
             {
                 uri: buildUri
             }
@@ -42,6 +44,21 @@ async function run() {
 
         let buildResponse: any = await rpn(buildOptions);
         task.debug(JSON.stringify(buildResponse));
+        */
+
+
+
+        // ideally from config
+        let token = task.getVariable('AgentCapabilitiesAccessToken');
+
+        let authHandler = vsts.getPersonalAccessTokenHandler(token);
+        let connection = new vsts.WebApi(collectionUri, authHandler);
+
+        let api = await connection.getBuildApi();
+        let build = await api.getBuild(buildId, projectId);
+
+        task.debug(build.queue.name);
+        task.debug(build.queue.pool.name);
 
         task.setResult(task.TaskResult.Succeeded, 'Succeeded');
     }
