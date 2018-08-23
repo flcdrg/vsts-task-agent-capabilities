f945bc1ad48407d6b2266c64436337173f88a3cd
Fernfrost <martin.nilsson@fernfrost.com>
2018-08-23 14:45:54 +0200

    adds connection error handling

--- agent-capabilities-task/index.ts
+++ agent-capabilities-task/index.ts
@@ -6,24 +6,35 @@ async function run() {
         let token = task.getInput('personalAccessToken', true);
 
         let collectionUri = task.getVariable('system.teamFoundationCollectionUri');
+
+        let authHandler = vsts.getPersonalAccessTokenHandler(token);
+
+        let connection: vsts.WebApi;
+        try {
+            connection = new vsts.WebApi(collectionUri, authHandler);
+        }
+        catch (err) {
+            throw new Error('Invalid Personal access token');
+        }
+
+
+
         let projectId = task.getVariable('system.teamProjectId');
         let buildId: number = Number(task.getVariable('build.buildId'));
 
-        let authHandler = vsts.getPersonalAccessTokenHandler(token);
-        let connection = new vsts.WebApi(collectionUri, authHandler);
 
-        let api = await connection.getBuildApi();
-        let build = await api.getBuild(buildId, projectId);
+        let buildApi = await connection.getBuildApi();
+        let build = await buildApi.getBuild(buildId, projectId);
 
         let poolId = build.queue.pool.id;
 
         let agentId = Number(task.getVariable('agent.id'));
 
-        let poolApi = await connection.getTaskAgentApi();
-        let agent = await poolApi.getAgent(poolId, agentId, true);
+        let agentApi = await connection.getTaskAgentApi();
+        let agent = await agentApi.getAgent(poolId, agentId, true);
         let keys = Object.keys(agent.systemCapabilities);
 
-        for (let key of keys){
+        for (let key of keys) {
             task.debug(key + ': ' + agent.systemCapabilities[key]);
 
             task.setVariable('agentCapabilities.' + key, agent.systemCapabilities[key]);
