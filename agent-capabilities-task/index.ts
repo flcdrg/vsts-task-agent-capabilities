acdaa4bead91c791f1e865643ecce2d2f232af7d
David Gardiner <david@gardiner.net.au>
2018-12-06 14:32:11 +1030

    Add support for userCapabilities
    - Implement #3

--- agent-capabilities-task/index.ts
+++ agent-capabilities-task/index.ts
@@ -1,5 +1,6 @@
 import task = require('azure-pipelines-task-lib/task');
 import * as vsts from 'vso-node-api';
+import { TaskAgent } from 'vso-node-api/interfaces/TaskAgentInterfaces';
 
 async function run() {
     try {
@@ -15,31 +16,35 @@ async function run() {
         let buildId: number = Number(task.getVariable('build.buildId'));
         let agentId = Number(task.getVariable('agent.id'));
 
-        let systemCapabilities;
-
+        let agent: TaskAgent;
         try{
             let buildApi = await connection.getBuildApi();
             let build = await buildApi.getBuild(buildId, projectId);
             let poolId = build.queue.pool.id;
             let agentApi = await connection.getTaskAgentApi();
-            let agent = await agentApi.getAgent(poolId, agentId, true);
-            systemCapabilities = agent.systemCapabilities;
+            agent = await agentApi.getAgent(poolId, agentId, true);
         }
         catch(err){
+            task.debug(`Error getting agent: ${err}`);
             throw new Error('Invalid personal access token. Make sure the token is valid and active.');
         }
-
-        let keys = Object.keys(systemCapabilities);
 
         console.log('Agent capability variables and values. Format: variable=value');
         console.log();
 
-        for (let key of keys) {
-            let formattedKey = `AgentCapabilities.${key}`;
-            let value = systemCapabilities[key];
-            task.setVariable(formattedKey, value);
-            console.log(`${formattedKey}=${value}`);
+        function setTaskVariables(kind: string, capabilities: any) {
+            const keys = Object.keys(capabilities);
+
+            for (const key of keys) {
+                const formattedKey = `AgentCapabilities.${kind}.${key}`;
+                const value = capabilities[key];
+                task.setVariable(formattedKey, value);
+                console.log(`${formattedKey}=${value}`);
+            }
         }
+
+        setTaskVariables('System', agent.systemCapabilities);
+        setTaskVariables('User', agent.userCapabilities);
 
         task.setResult(task.TaskResult.Succeeded, 'Succeeded');
     }
